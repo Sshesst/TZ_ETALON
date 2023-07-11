d48d01ece770c077841192b2708ff091688a61ff
Sshesst <dimciksalkov1832@gmail.com>
2023-07-11 13:30:37 +0500

    change createController

--- controllers/clientCreateBookingController.js
+++ controllers/clientCreateBookingController.js
@@ -1,39 +1,49 @@
 const Room = require('../models/Room');
 const Booking = require('../models/Booking');
 const AvailableDate = require("../models/AvailableDate");
+const { Transaction } = require('sequelize');
+const sequelize = require('../db');
 
 class clientCreateBookingController {
     async createBooking(req, res) {
+        const { startDate, endDate, RoomId, ClientId } = req.body;
+
         try {
-            const { startDate, endDate, RoomId, ClientId } = req.body;
-
-            const availableRooms = await AvailableDate.findAll({
-                where: {
-                    startDate: startDate,
-                    endDate: endDate
-                },
-                include: [{ model: Room }]
-            });
-
-            if (availableRooms.length > 0) {
-                const booking = await Booking.create({
-                    startDate,
-                    endDate,
-                    RoomId,
-                    ClientId,
-                });
-
-                await AvailableDate.destroy({
+            await sequelize.transaction(async (t) => {
+                const availableRooms = await AvailableDate.findAll({
                     where: {
                         startDate: startDate,
                         endDate: endDate
-                    } });
+                    },
+                    include: [{ model: Room }],
+                    transaction: t
+                });
 
-                res.json({message: 'комната успешно забронирована'})
-            } else {
-                res.status(404).json({ message: 'Нет доступных комнат' });
-            }
+                if (availableRooms.length > 0) {
+                    const booking = await Booking.create(
+                        {
+                            startDate,
+                            endDate,
+                            RoomId,
+                            ClientId
+                        },
+                        { transaction: t }
+                    );
 
+                    await AvailableDate.destroy({
+                        where: {
+                            startDate: startDate,
+                            endDate: endDate
+                        },
+                        transaction: t
+                    });
+
+                    res.json({ message: 'комната успешно забронирована' });
+
+                } else {
+                    res.status(404).json({ message: 'Нет доступных комнат' });
+                }
+            });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Ошибка сервера' });
--- middleware/checkBookingMiddleware.js
+++ middleware/checkBookingMiddleware.js
@@ -1,21 +1,26 @@
-const { Op } = require('sequelize');
+const { Op, Transaction } = require('sequelize');
 const Booking = require('../models/Booking');
+const sequelize = require('../db');
 
 async function checkBookingMiddleware(req, res, next) {
     const { startDate, endDate, RoomId } = req.body;
 
     try {
-        const existingBooking = await Booking.findOne({
-            where: {
-                RoomId,
-                startDate: { [Op.lt]: endDate },
-                endDate: { [Op.gt]: startDate }
+        await sequelize.transaction(async (t) => {
+            const existingBooking = await Booking.findOne({
+                where: {
+                    RoomId,
+                    startDate: { [Op.lt]: endDate },
+                    endDate: { [Op.gt]: startDate }
+                },
+                transaction: t,
+                lock: Transaction.LOCK.UPDATE
+            });
+
+            if (existingBooking) {
+                return res.status(400).json({ error: 'Номер комнаты уже забронирован на указанный период' });
             }
         });
-
-        if (existingBooking) {
-            return res.status(400).json({ error: 'Номер комнаты уже забронирован на указанный период' });
-        }
 
         next();
     } catch (error) {
